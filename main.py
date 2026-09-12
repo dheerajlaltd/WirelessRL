0f8b6bc95df6687c978ec08475599926d80dc044
dheerajlaltd <laldheerajtd@gmail.com>
2026-09-12 13:47:09 +0530

    Update main.py
    User region changes according to the mobility model in this version of main

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from src.enums import Region
 from src.user import User
+from src.mobility import Random_walk_mobility
 
 def main():
+    # Creation of a user in the centre region
     user=User(Region.CENTRE)
+    print("\n\nWELCOME TO RL WORLD\n")
+    # Creation of the mobility model
+    mobility=Random_walk_mobility()
     print('Initial Position')
     user.display()
     print('\nUser Movement')
-    for step in range(10):
-        user.move()
-        print(f"Step {step+1}:",end="")
+    for step in range(20):
+        mobility.move(user)
+        print(f"Time Step {step+1}:",end="")
         user.display()
 if __name__=="__main__":
     main()
