c981f430de1cd4ef1dbb6bfb88e8546e3144b366
dheerajlaltd <laldheerajtd@gmail.com>
2026-09-12 15:17:24 +0530

    Update main.py
    Addition of Beam and BaseStation in to the program

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.enums import Region
 from src.user import User
 from src.mobility import Random_walk_mobility
+from src.base_station import BaseStation
 
 def main():
     # Creation of a user in the centre region
@@ -8,8 +9,12 @@ def main():
     print("\n\nWELCOME TO RL WORLD\n")
     # Creation of the mobility model
     mobility=Random_walk_mobility()
+    # Creation of base station with 3 beams
+    base_station=BaseStation(num_beams=3)
     print('Initial Position')
     user.display()
+    print()
+    base_station.display_beams()
     print('\nUser Movement')
     for step in range(20):
         mobility.move(user)
